7dcafb381b0e6c3e1390609d8c3fa0361c12c689
loreto <loreto>
2025-02-08 17:37:03 +0100

    starting sport facilities

--- time2sport/gestion_deportiva/urls.py
+++ time2sport/gestion_deportiva/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import all_activities, activity_detail
+from .views import all_activities, activity_detail, all_facilities, facility_detail
 
 urlpatterns = [
     path('activities/', all_activities, name='all_activities'),
     path('activities/<int:activity_id>/', activity_detail, name='activity_detail'),
+    path('facilities/', all_facilities, name='all_facilities'),
+    path('facilities/<int:facility_id>/', facility_detail, name='facility_detail'),
 ]
--- time2sport/gestion_deportiva/views.py
+++ time2sport/gestion_deportiva/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Activity
+from .models import Activity, SportFacility
 
 def all_activities(request):
     activities = Activity.objects.prefetch_related('photos').all()
@@ -8,3 +8,11 @@ def all_activities(request):
 def activity_detail(request, activity_id):
     activity = get_object_or_404(Activity, pk=activity_id)
     return render(request, 'activities/activity_detail.html', {'activity': activity})
+
+def all_facilities(request):
+    facilities = SportFacility.objects.prefetch_related('photos').all()
+    return render(request, 'facilities/all_facilities.html', {'facilities': facilities})
+
+def facility_detail(request, facility_id):
+    facility = get_object_or_404(SportFacility, pk=facility_id)
+    return render(request, 'facilities/facility_detail.html', {'facility': facility})
