3db7b6755e77ed1c5ce3a09a17b5fed9741cc510
esthermtnz <esther.martinezblanco@estudiante.uam.es>
2025-02-11 22:35:57 +0100

    Home page defined and correct user redirect after login

--- time2sport/gestion_deportiva/urls.py
+++ time2sport/gestion_deportiva/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
+    path('home/', views.index, name='index'),
 ]
--- time2sport/gestion_deportiva/views.py
+++ time2sport/gestion_deportiva/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
 # Create your views here.
 def login(request):
     context = {}
     return render(request, 'gestion_deportiva/login.html', context)
+
+
+def index(request):
+    context = {}
+    return render(request, 'gestion_deportiva/home.html', context)
 
 from django.conf import settings
 ms_identity_web = settings.MS_IDENTITY_WEB
