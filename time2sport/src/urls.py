2128f314f34dfff7f78d9731768bafdb59188818
esthermtnz <esther.martinezblanco@estudiante.uam.es>
2025-04-07 17:24:46 +0200

    Add tab template reservations for consulting past, future and waiting list reservations

--- time2sport/src/urls.py
+++ time2sport/src/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import reserve_activity_session, purchase_bonus, check_reserve_facility_session
+from .views import reserve_activity_session, purchase_bonus, check_reserve_facility_session,reservations, past_reservations
 
 urlpatterns = [
     path('reserve/<int:session_id>/', reserve_activity_session, name='reserve_activity_session'),
     path('purchase_bonus/<int:bonus_id>/', purchase_bonus, name='purchase_bonus'),
     path('reserve/', check_reserve_facility_session, name='check_reserve_facility_session'),
+    path('reservations/', reservations, name='reservations'),
+    path('past-reservations/', past_reservations, name='past-reservations'),
 ]
